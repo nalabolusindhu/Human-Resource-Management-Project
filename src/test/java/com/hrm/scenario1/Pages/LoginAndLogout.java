d8342ba7c4d136fb89bcdad7848c944bd42502e0
Nalabolu Sindhu <nalabolusindhu02@gmail.com>
2025-06-11 20:09:11 +0530

    HRM

--- src/test/java/com/hrm/scenario1/Pages/LoginAndLogout.java
+++ src/test/java/com/hrm/scenario1/Pages/LoginAndLogout.java
@@ -1,49 +1,49 @@
 package com.hrm.scenario1.Pages;
 
-import org.testng.annotations.Test;
-
-import org.openqa.selenium.By;
-import org.testng.Assert;
-import org.testng.annotations.Listeners;
-
-import com.hrm.scenario1.BaseClass;
-import com.hrm.scenario1.ExtentITestNGListener;
-import com.hrm.scenario1.Utility;
-
-@Listeners(ExtentITestNGListener.class)
-public class LoginAndLogout extends BaseClass {
-
-	By userName = By.name("username");
-	By password = By.xpath("//input[@placeholder='Password']");
-	By loginBtn = By.xpath("//button[text()=' Login ']");
-
-	@Test(dataProvider = "Excel", dataProviderClass = Utility.class)
-	public void loginWithValidAndInvalidCred(String uid, String psw) throws InterruptedException {
-
-        driver.findElement(userName).sendKeys(uid);
-        driver.findElement(password).sendKeys(psw);
-        driver.findElement(loginBtn).click();
-        Thread.sleep(1500);
-
-        Utility util = new Utility();
-        util.captureScreenshot(driver, uid + "_" + psw + "_");
-
-        String currentUrl = driver.getCurrentUrl();
-        boolean isLoginSuccessful = currentUrl.contains("dashboard");
-
-        if (uid.equalsIgnoreCase("Admin") && psw.equals("admin123")) {
-            Assert.assertTrue(isLoginSuccessful, "Expected valid login but failed");
-            System.out.println("Login successful and landed on dashboard");
-            logout();
-        } else {
-            Assert.assertFalse(isLoginSuccessful, "Expected login to fail, but passed");
-            System.out.println("Login failed as expected for invalid credentials");
-        }
-    }
-
-    public void logout() throws InterruptedException {
-        driver.findElement(By.xpath("//i[contains(@class,'oxd-userdropdown-icon')]")).click();
-        driver.findElement(By.linkText("Logout")).click();
-        Thread.sleep(2000);
-    }
+import org.openqa.selenium.By; 
+import org.testng.Assert; 
+import org.testng.annotations.Listeners; 
+import org.testng.annotations.Test; 
+ 
+import com.hrm.scenario1.BaseClass; 
+import com.hrm.scenario1.ExtentITestNGListener; 
+import com.hrm.scenario1.Utility; 
+ 
+@Listeners(ExtentITestNGListener.class) 
+public class LoginAndLogout extends BaseClass { 
+ 
+ By userName = By.name("username"); 
+ By password = By.xpath("//input[@placeholder='Password']"); 
+ By loginBtn = By.xpath("//button[text()=' Login ']"); 
+ 
+ @Test(dataProvider = "Excel", dataProviderClass = Utility.class) 
+ public void loginWithValidAndInvalidCred(String uid, String psw) 
+throws InterruptedException { 
+ 
+  driver.findElement(userName).sendKeys(uid); 
+  driver.findElement(password).sendKeys(psw); 
+  driver.findElement(loginBtn).click(); 
+  Thread.sleep(1500); 
+  System.out.println("Username : " + uid + " & Password : " + 
+psw); 
+ 
+  Utility util = new Utility(); 
+  util.captureScreenshot(driver, uid + "_" + psw + "_"); 
+ 
+  System.out.println("Logged in"); 
+ 
+ 
+ Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"), 
+"Login Failed......."); 
+  System.out.println("Login successful!!!!!!!"); 
+  logout(); 
+ 
+ } 
+ 
+ public void logout() throws InterruptedException { 
+  driver.findElement(By.xpath("//i[contains(@class,'oxd-userdropdown-icon')]")).click(); 
+  driver.findElement(By.linkText("Logout")).click(); 
+  Thread.sleep(2000); 
+ } 
+ 
 }
